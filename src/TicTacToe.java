5e6b04b6e9f7dcbe3af102a6b385fb23b64e87c0
nkasaby <n.kasaby@aucegypt.edu>
2023-06-08 23:45:01 +0300

    printing gameboard with each field marked with its position + choosing X or O depending on user

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,70 @@
+import java.util.Scanner;
+
 public class TicTacToe {
-    public static void main(String [] args){
+    public static void main(String[] args) {
+        char[][] gameBoard = {
+                {'1', '|', '2', '|', '3'},
+                {'-', '-', '-', '-', '-'},
+                {'4', '|', '5', '|', '6'},
+                {'-', '-', '-', '-', '-'},
+                {'7', '|', '8', '|', '9'}};
+        printGameBoard(gameBoard);
+        choosePosition(gameBoard, "computer");
 
     }
+
+    public static void printGameBoard(char[][] board) {
+
+        for (char[] row : board) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void choosePosition(char[][] board, String player) {
+        System.out.print("Please enter the position of your choice from 1-9:");
+        Scanner positionChoice = new Scanner(System.in);
+        int position = positionChoice.nextInt();
+
+        char piece = ' ';
+
+        if (player.equals("player1"))
+            piece = 'X';
+        if (player.equals("computer"))
+            piece = 'O';
+
+        if (position == 1)
+            board[0][0] = piece;
+        else if (position == 2)
+            board[0][2] = piece;
+        else if (position == 3)
+            board[0][4] = piece;
+        else if (position == 4)
+            board[2][0] = piece;
+        else if (position == 5)
+            board[2][2] = piece;
+        else if (position == 6)
+            board[2][4] = piece;
+        else if (position == 7)
+            board[4][0] = piece;
+        else if (position == 8)
+            board[4][2] = piece;
+        else if (position == 9)
+            board[4][4] = piece;
+
+        printGameBoard(board);
+
+    }
+
+
+
+
+
+
+
+
+
 }
+
