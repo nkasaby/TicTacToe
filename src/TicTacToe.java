32a37ea2b07940385255f2150b2b419189171a16
nkasaby <n.kasaby@aucegypt.edu>
2023-06-09 02:04:27 +0300

    added the resuts + validations

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,9 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class TicTacToe {
+    static Vector <Byte> player1Positions = new Vector<>();
+    static Vector <Byte> computerPositions = new Vector<>();
+
     public static void main(String[] args) {
         char[][] gameBoard = {
                 {'1', '|', '2', '|', '3'},
@@ -11,8 +13,25 @@ public class TicTacToe {
                 {'7', '|', '8', '|', '9'}};
         printGameBoard(gameBoard);
         System.out.println();
-        choosePosition(gameBoard, "player1");
+        String result = winner();
+        while(winner().equals("")) {
+            System.out.println("Player 1's turn:");
+            choosePosition(gameBoard, "player1");
+            System.out.println();
 
+            result = winner();
+            if (!result.equals("")) {
+                System.out.println(result);
+                break;
+            }
+
+            System.out.println("Computer's turn:");
+            choosePosition(gameBoard, "computer");
+            System.out.println();
+
+            result = winner();
+            System.out.println(result);
+        }
     }
 
     public static void printGameBoard(char[][] board) {
@@ -36,11 +55,30 @@ public class TicTacToe {
         if (player.equals("player1")) {
             System.out.print("Please enter the position of your choice from 1-9:");
             Scanner player1PositionChoice = new Scanner(System.in);
-            position = player1PositionChoice.nextByte();
+            byte player1Position = player1PositionChoice.nextByte();
+            position = player1Position;
+
+            while (player1Positions.contains(position)||computerPositions.contains(position)){
+                System.out.println("Position is occupied, please choose another one.");
+                player1Position = player1PositionChoice.nextByte();
+                position = player1Position;
+
+            }
+
+            player1Positions.add(player1Position);
             piece = 'X';
+            System.out.println("p1 positions: " + player1Positions);
         }
-        if (player.equals("computer")){
+        else if (player.equals("computer")){
             position = computerPosition;
+
+            while (player1Positions.contains(position)||computerPositions.contains(position)){
+                System.out.println(" COMPUTER Position is occupied, please choose another one.");
+                computerPosition = (byte) (computerPositionChoice.nextInt(9) + 1);
+                position = computerPosition;
+            }
+            computerPositions.add(position);
+            System.out.println("comp positions: " + computerPositions);
             piece = 'O';
         }
 
@@ -66,7 +104,28 @@ public class TicTacToe {
 
     }
 
+  public static String winner (){
 
+      List<List<Byte>> conditions = new ArrayList<>();
+      conditions.add(Arrays.asList((byte) 1, (byte) 2, (byte) 3));
+      conditions.add(Arrays.asList((byte) 4, (byte) 5, (byte) 6));
+      conditions.add(Arrays.asList((byte) 7, (byte) 8, (byte) 9));
+      conditions.add(Arrays.asList((byte) 1, (byte) 4, (byte) 7));
+      conditions.add(Arrays.asList((byte) 2, (byte) 5, (byte) 8));
+      conditions.add(Arrays.asList((byte) 3, (byte) 6, (byte) 9));
+      conditions.add(Arrays.asList((byte) 1, (byte) 5, (byte) 9));
+      conditions.add(Arrays.asList((byte) 3, (byte) 5, (byte) 7));
+
+      for (List<Byte> l : conditions) {
+          if (player1Positions.containsAll(l))
+              return "Player 1 wins!";
+          else if (computerPositions.containsAll(l))
+              return "Computer wins :(";
+      }
+          if (player1Positions.size() + computerPositions.size() == 9)
+             return "Tie";
+   return "";
+  }
 
 
 
